0a6163750f1104205a25125cd0d38585000ac6c9
Rustin Manafian <rustin.manafian@lookout.com>
2013-08-30 14:29:19 -0700

    Some stuff was changed.

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,5 +1,6 @@
 import fi.iki.elonen.NanoHTTPD;
 
+import com.google.gson.Gson;
 import java.util.Map;
 import com.android.uiautomator.core.UiDevice;
 
@@ -14,21 +15,52 @@ public class Server extends NanoHTTPD {
         if(/*method == Method.POST && */uri.equalsIgnoreCase("/execute")) {
             return execute(params);
         }
+        if(method == Method.GET && uri.equalsIgnoreCase("/clean")) {
+            return clean();
+        }
 
         return new Response("no dice grandma");
     }
 
+    public Response clean() {
+        return new Response("Cleaning successful!");
+    }
+
     public Response execute(Map<String,String> params) {
+        //Get this parameter shit to work
+        //Do something about having persistent values
+        //Do something about boolean return values
         try {
             String clas = "com.android.uiautomator.core." + params.get("class");
-            String method = params.get("method");
             Class c = Class.forName(clas);
-            c.getDeclaredMethod(method).invoke(UiDevice.getInstance());
+            String meth = params.get("method");
+            java.lang.reflect.Method[] methods = c.getDeclaredMethods();
+            java.lang.reflect.Method method = null;
+            Class<?>[] parameterTypes = null;
+            String argumentJson = params.get("arguments");
+            Object[] args = new Gson().fromJson(argumentJson,Object[].class);
+            for(java.lang.reflect.Method m : methods) {
+                Class<?>[] pt = m.getParameterTypes();
+                if(m.getName().equalsIgnoreCase(meth) && pt.length == args.length) {
+                    method = m;
+                    parameterTypes = pt;
+                    break;
+                }
+            }
+
+            for(int i = 0; i < args.length; i++) {
+                //Dealing with primitives
+                if(args[i] instanceof Double) {
+
+                } else {
+                    args[i] = parameterTypes[i].cast(args[i]);
+                }
+            }
+            Object o = method.invoke(UiDevice.getInstance(),args);
+
             return new Response("Shits working.");
         } catch(Exception e) {
             System.out.println(e);
-            System.out.println(e.getMessage());
-            System.out.println(e.getCause());
             return new Response("Somebody fucked up.");
         }
     }
